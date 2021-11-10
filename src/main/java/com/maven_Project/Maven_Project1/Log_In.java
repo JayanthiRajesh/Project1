cac15cd3f735c86adf1d72e62f626d569735b5fa
JayanthiThiruppathi <jayanthi1225@gmail.com>
2021-11-10 11:58:44 +0530

    POM class

---
+++ src/main/java/com/maven_Project/Maven_Project1/Log_In.java
@@ -0,0 +1,42 @@
+package com.maven_Project.Maven_Project1;
+
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class Log_In {
+	public WebDriver driver;
+	@FindBy(xpath="//input[@name='username']")
+	private WebElement username;
+	
+	@FindBy(xpath="//input[@name='password']") private WebElement password;
+	
+	public WebElement getUsername() {
+		return username;
+	}
+
+	public WebElement getPassword() {
+		return password;
+	}
+
+	public WebElement getLogin() {
+		return login;
+	}
+
+	@FindBy(id="login") private WebElement login;
+	
+	public Log_In(WebDriver driver) {
+		this.driver=driver;
+		PageFactory.initElements(driver, this);
+	}
+	
+	
+
+}
+
+
+	
+
